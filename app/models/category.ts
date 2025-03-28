c915d46c356b3d65dd9419e856978ad4754047c7
alfredogust <alfredog.fu@gmail.com>
2025-03-28 16:29:38 -0300

    feat: created category model

---
+++ app/models/category.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Category extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare active: boolean
+
+  @column()
+  declare icon_name: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
