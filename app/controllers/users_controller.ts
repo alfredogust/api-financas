f6013d588766ea186214417d8be529490f09f628
alfredogust <alfredog.fu@gmail.com>
2025-03-29 14:27:00 -0300

    feat: add update method to UsersController

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,6 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import User from '#models/user'
 import hash from '@adonisjs/core/services/hash'
+import { hasOnlyExpressionInitializer } from 'typescript'
 
 export default class UsersController {
 
@@ -18,5 +19,35 @@ export default class UsersController {
 
     const user = await User.create(data)
     return response.created(user)
+  }
+
+  // Exibe um usuário
+  async show({ params, response }: HttpContext) {
+    const user = await User.find(params.id)
+
+    if (!user) {
+      return response.notFound({ message: 'User not found.' })
+    }
+
+    return response.ok(user)
+  }
+
+  async update({ params, request, response }: HttpContext) {
+    const user = await User.find(params.id)
+
+    if (!user) {
+      return response.notFound({ message: 'User not found.' })
+    }
+
+    const data = request.only(['name', 'email', 'password', 'salary'])
+
+    if (data.password) {
+      data.password = await hash.make(data.password)
+    }
+
+    user.merge(data)
+    await user.save()
+
+    return response.ok(user)
   }
 }
